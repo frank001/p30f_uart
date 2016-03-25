dfe8ac673f66ce06042620b0e8fe46c5a6ced909
frans <frans@neptune.zwerver.local>
2016-03-25 15:23:11 +0100

    circular buffers for rx and tx

--- interrupt.c
+++ interrupt.c
@@ -2,13 +2,30 @@
 #include "interrupt.h"
 #include "websocket.h"
 
-unsigned char buffer[255];
+unsigned char rxBuffer[MAXBYTES];
+unsigned char rxPtr = 0;
+
+unsigned char txBuffer[MAXBYTES];
+unsigned char txPtr = 0;
+extern unsigned char writePtr;
+
 unsigned char tempRX2;
-unsigned char byteCount =0;
+unsigned char byteCount = 0;
+
 
 void __attribute__((__interrupt__, __auto_psv__)) _U2RXInterrupt(void) {
     tempRX2 = U2RXREG;
-    ReadClient(tempRX2);
-    //buffer[byteCount++] = tempRX2;
-    IFS1bits.U2RXIF = 0;
+    rxBuffer[rxPtr++] = tempRX2;
+    rxPtr%=MAXBYTES;
+    IFS1bits.U2RXIF = 0;        //clear interrupt flag
+}
+
+void __attribute__((__interrupt__, __auto_psv__)) _U2TXInterrupt(void) {
+    while (txPtr!=writePtr) {
+        U2TXREG = txBuffer[txPtr++];
+        while (!U2STAbits.TRMT) ;
+        txPtr%=MAXBYTES;
+    }
+    IFS1bits.U2TXIF = 0;    //clear interrupt flag
+    
 }
