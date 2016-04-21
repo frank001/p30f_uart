2152dfa76eca738956efde8620bad0da6b7b4e18
frans <frans@neptune.zwerver.local>
2016-04-21 20:34:00 +0200

    Overall improvement

--- uart.c
+++ uart.c
@@ -9,32 +9,29 @@ unsigned char txBuffer[MAXBYTES];
 unsigned char txPtr = 0;
 extern unsigned char writePtr;
 
-void initUART(void) {
-    //initialize UART2
-    _TRISF5 = 0;        //UART2 TX
-    _TRISF4 = 1;        //UART2 RX
-    U2BRG = UBRG2_VALUE;
-    IEC1bits.U2RXIE=1;
-    IEC1bits.U2TXIE=1;
-    U2STA&=0xfffc;
-    U2MODEbits.UARTEN=1;
-    U2STAbits.UTXEN=1;
-}
+void initUART(void) {       //initialize UART1
+    _TRISF3 = 0;            //UART1 TX
+    _TRISF2 = 1;            //UART1 RX
+    U1BRG = UBRG1_VALUE;
+    IEC0bits.U1RXIE=1;
+    U1STA&=0xfffc;
+    U1MODEbits.UARTEN=1;
+    U1STAbits.UTXEN=1;
+ }
 
 void Commit(void) {
-    while (IFS1bits.U2TXIF) ;       //wait until clear to transmit
-    IFS1bits.U2TXIF = 1;            //raise interrupt, the routine will take care of the rest
+    IEC0bits.U1TXIE=1;                //enable TX interrupt
 }
 
-void __attribute__((__interrupt__, __no_auto_psv__)) _U2RXInterrupt(void) {
-        rxBuffer[rxPtr++] = U2RXREG;
+void __attribute__((__interrupt__, __no_auto_psv__)) _U1RXInterrupt(void) {
+        rxBuffer[rxPtr++] = U1RXREG;
         rxPtr%=MAXBYTES;
-        IFS1bits.U2RXIF = 0;        //clear interrupt flag
+        IFS0bits.U1RXIF = 0;        //clear interrupt flag
 }
 
-void __attribute__((__interrupt__, __no_auto_psv__)) _U2TXInterrupt(void) {
-        U2TXREG = txBuffer[txPtr++];
-        txPtr%=MAXBYTES;
-        while (!U2STAbits.TRMT) ;
-        if (txPtr==writePtr) IFS1bits.U2TXIF = 0;    //clear interrupt flag
+void __attribute__((__interrupt__, __no_auto_psv__)) _U1TXInterrupt(void) {
+    U1TXREG = txBuffer[txPtr++];
+    txPtr%=MAXBYTES;
+    while (!U1STAbits.TRMT) ;
+    if (txPtr==writePtr) IEC0bits.U1TXIE=0;     //finished writing, disable TX interrupt
 }
