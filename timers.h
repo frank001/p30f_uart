f030e8fdffd1e956f398043b73d4a5e21ef2a1a4
frans <frans@neptune.zwerver.local>
2016-04-21 07:16:18 +0200

    Overall improvement

---
+++ timers.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   timers.h
+ * Author: frans
+ *
+ * Created on 15 april 2016, 16:59
+ */
+
+#ifndef TIMERS_H
+#define	TIMERS_H
+
+void startTimer4(void);
+void stopTimer4(void);
+void initTimer4(void);
+
+#endif	/* TIMERS_H */
+
