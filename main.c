ef23e5e0eafd6a237f47fbdb1278fe363cc04450
frans <frans@neptune.zwerver.local>
2016-04-21 07:15:02 +0200

    Overall improvement

--- main.c
+++ main.c
@@ -3,15 +3,20 @@
 #include <p30f4013.h>
 #include <uart.h>
 #include "config.h"
-#include "interrupt.h"
+#include "uart.h"
+#include "timers.h"
+#include "adc.h"
 
 extern unsigned char txBuffer[];
 extern unsigned char txPtr;
 
 unsigned char isConnected = 0;
 
-unsigned char hello[] ="Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!";
 
+
+//unsigned char hello[] ="Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!!!!";
+unsigned char hello[] ="Hello World!Hello World!Hello W";
+extern unsigned int ADC10;
 
 void delay(unsigned int d) {
     __delay32(d*10000);
@@ -19,60 +24,72 @@ void delay(unsigned int d) {
 
 
 int main(void) {
-    //TRISB = 0x10;
     int i;
-    int timeOut;
-    char flipbit;
-    _TRISF5 = 0;        //UART2 TX
-    _TRISF4 = 1;        //UART2 RX
+    char flipbit=0;
+    char senddata=0;
+    unsigned int adc10;
     
+
     TRISB = 0;
+    
+    _TRISB10 = 1;
+    
+    
+    
     _LATD2 = 1;
     _TRISD2 = 1;
     
+    initADC();
+    initUART();
     
-    unsigned char tx;
+    initTimer4();
+    startTimer4();
     
-    U2BRG = UBRG2_VALUE;
-    
-    IEC1bits.U2RXIE=1;
-    IEC1bits.U2TXIE=1;
-    U2STA&=0xfffc;
-    U2MODEbits.UARTEN=1;
-    U2STAbits.UTXEN=1;
-    
-    
+    adc10 = ADC10;
     while (1) {
-        
-        if (_RD2==0 ) {
-            if (!flipbit) DELAY_MS(100);
-            if (_RD2==0) {
+        if (!flipbit && _RD2==0) {
+            DELAY_MS(100);
+            if (_RD2==0) { 
+                senddata=!senddata;
                 flipbit = 1;
-                                                //TODO: don't send when not connected!
-                while (IFS1bits.U2TXIF) ;       //wait until clear to transmit
-                //IEC1bits.U2TXIE=0;
+            }
+        }
+        if (flipbit && _RD2==1) {
+            DELAY_MS(100);
+            if (_RD2==1) flipbit = 0;
+        }
+        _LATB3 = senddata;        
+
+        if (isConnected) {
+            if (adc10!=ADC10) {
+                adc10 = ADC10;
+                WriteWebSocket(0x82);           //FIN bit high and opcode=2 (binary)
+                WriteWebSocket(0x02);  //payload length
+                //for (i=0;i<sizeof(adc10);i++) 
+                WriteWebSocket(adc10 & 0x00ff);
+                WriteWebSocket((adc10 & 0xff00)>>7);
+                Commit();
+            }
+            
+            if (senddata) {
                 WriteWebSocket(0x81);           //FIN bit high and opcode=1
                 WriteWebSocket(sizeof(hello));  //payload length
                 for (i=0;i<sizeof(hello);i++) 
                     WriteWebSocket(hello[i]);
-                //IEC1bits.U2TXIE=1;
-                U2TXREG = txBuffer[txPtr++];    //write the first byte
-                
-                _LATB3 = 1;
+                Commit();
+                //_LATB3 = 1;
             }
+            /*else { 
+                //_LATB3 = 1;
+                if (flipbit) DELAY_MS(100);
+                if (_RD2==1){
+                    flipbit=0;
+                    _LATB3 = 0;
+                }
+            }*/
         }
-        else { 
-            _LATB3 = 0;
-            flipbit=0;
-        }
-        DELAY_MS(15);
+        DELAY_MS(10);
         ReadWebSocket();    
-        
-        //_LATB0 = 1;
-        //DELAY_MS(1000);
-        //_LATB0 = 0;
-        //DELAY_MS(1000);
-        
     }
     
 }
